bd006d1ead2b4b2bbfe1aa2ffc3efea13f723ba4
DESKTOP-1VBB2KE\admin <howmun.lee@trealtechnology.com>
2021-05-14 16:24:07 +0800

    completed checkout flow

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework import serializers, status
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-from rest_framework.serializers import Serializer
-
 from ..models import Product
 from base.serializers import ProductSerializer
-from base.products import products
 
 
 @api_view(['GET'])

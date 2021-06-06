a32d5809b55e834e91be2f2cc740315ed753e080
DESKTOP-1VBB2KE\admin <howmun.lee@trealtechnology.com>
2021-06-06 18:54:06 +0800

    added admin - product module

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -3,5 +3,10 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name="products"),
+    # has to be above <str:pk>, else will map it to str:pk
+    path('create/', views.createProduct, name="product-create"),
+    path('upload/', views.uploadImage, name="image-upload"),
     path('<str:pk>/', views.getProduct, name="product"),
+    path('update/<str:pk>/', views.updateProduct, name="product-update"),
+    path('delete/<str:pk>/', views.deleteProduct, name="product-delete"),
 ]
